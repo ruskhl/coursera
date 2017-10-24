459e8b1ec1b92bfea1ee0e81ae23a13c3a8677b3
ruslana <ruslana.khlopuk@gmail.com>
2017-10-24 16:02:26 +0200

    ok

--- site/modern/js/script.js
+++ site/modern/js/script.js
@@ -1,10 +1,19 @@
-$(function () {
-	$("#navbarToggle").blur(function(event) {
-		var screenWidth = window.innerWidth;
-		if (screenWidth < 768) {
-			$("#bs-example-navbar-collapse-1").collapse('hide');
-		}
+// $(function () {
+// 	$("#navbarToggle").blur(function(event) {
+// 		var screenWidth = window.innerWidth;
+// 		if (screenWidth < 768) {
+// 			$("#bs-example-navbar-collapse-1").collapse('hide');
+// 		}
 
-	});
+// 	});
 	
-});
+// });
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "navbar") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "navbar";
+//     }
+// }
