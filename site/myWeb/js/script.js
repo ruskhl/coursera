fa493323c44a145d90e1de98aa2690111bc9de8f
ruslana <ruslana.khlopuk@gmail.com>
2017-11-02 16:11:24 +0100

    ok

---
+++ site/myWeb/js/script.js
@@ -0,0 +1,19 @@
+// $(function () {
+// 	$("").blur(function(event) {
+// 		var screenWidth = window.innerWidth;
+// 		if (screenWidth < 768) {
+// 			$("#dropdown-menu").collapse('hide');
+// 		}
+
+// 	});
+	
+// });
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "navbar") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "navbar";
+//     }
+// }
