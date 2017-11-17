75442c13cc1c5f4c03809cb636544a446c0bfaaf
ruslana <ruslana.khlopuk@gmail.com>
2017-11-17 17:48:26 +0100

    *

--- site/modern2/js/script.js
+++ site/modern2/js/script.js
@@ -12,8 +12,8 @@ function myFunction(x) {
 }
 
 
-var sidebar = document.getElementById('sidebar');
-Stickyfill.add(sidebar);
+// var sidebar = document.getElementById('sidebar');
+// Stickyfill.add(sidebar);
 
 var elements = document.querySelectorAll('.sticky');
 Stickyfill.add(sidebar);
