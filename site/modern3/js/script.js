c1902b7c89e7a0ecf1108db183588f219d37691e
ruslana <ruslana.khlopuk@gmail.com>
2018-01-01 15:47:34 +0100

    Ok

--- site/modern3/js/script.js
+++ site/modern3/js/script.js
@@ -2,17 +2,17 @@
 
 
 
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset >= menuPosition.top && !isAdded) {
-        menu.classList.add('sticky');
-        menu.parentNode.insertBefore(placeholder, menu);
-        isAdded = true;
-    } else if (window.pageYOffset < menuPosition.top && isAdded) {
-        menu.classList.remove('sticky');
-        menu.parentNode.removeChild(placeholder);
-        isAdded = false;
-    }
-});
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset >= menuPosition.top && !isAdded) {
+//         menu.classList.add('sticky');
+//         menu.parentNode.insertBefore(placeholder, menu);
+//         isAdded = true;
+//     } else if (window.pageYOffset < menuPosition.top && isAdded) {
+//         menu.classList.remove('sticky');
+//         menu.parentNode.removeChild(placeholder);
+//         isAdded = false;
+//     }
+// });
 
 
 
